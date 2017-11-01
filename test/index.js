e2a8b30dad4e9787ee3371845d3a170f17d6496a
Brad Oyler <bradoyler@gmail.com>
2017-11-01 19:32:56 -0400

    lint refactor

--- test/index.js
+++ test/index.js
@@ -1,17 +1,27 @@
-var SheetReader = require('../index');
+const SheetReader = require('../index')
+
+// public: 1INGc-QFBfCS0raSZgprennLRvJ8YWJHCV4YmuEkYoP4
 
 SheetReader({
-    key: '1INGc-QFBfCS0raSZgprennLRvJ8YWJHCV4YmuEkYoP4'
-}, function(err, spreadsheet) {
-
-    spreadsheet.worksheets[0].rows({limit:5, excludeHeader:true
-    }, function(err, sheet1rows) {
-
-        console.log('Sheet1:', sheet1rows);
-
-        spreadsheet.worksheets[1].rows({}, function(err, sheet2rows) {
-            console.log('Sheet2:', sheet2rows);
-        });
-    });
-
-});
+  key: '1INGc-QFBfCS0raSZgprennLRvJ8YWJHCV4YmuEkYoP4'
+}, function (err, spreadsheet) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  spreadsheet.worksheets[0].rows({limit: 5, excludeHeader: true
+  }, function (err, sheet1rows) {
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.log('Sheet1:', sheet1rows)
+    spreadsheet.worksheets[1].rows({}, function (err, sheet2rows) {
+      if (err) {
+        console.log(err)
+        return
+      }
+      console.log('Sheet2:', sheet2rows)
+    })
+  })
+})
